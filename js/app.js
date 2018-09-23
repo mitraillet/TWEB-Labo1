47123917c9186ddcb491bb21bb1578bb45452dff
mitraillet <steve.henriquet@gmail.com>
2018-09-23 20:10:18 +0200

    Adding action on button read more and less

--- js/app.js
+++ js/app.js
@@ -8,5 +8,22 @@ function like(){
 
 
 function arrow(){
-  alert("You have share it !");
+  alert("You just share it !");
+}
+
+function show(){
+  document.getElementById('read').setAttribute("onclick","hide()");
+  document.getElementById('read').setAttribute("class","readOpen");
+  document.getElementsByClassName('text')[0].style.maxHeight = "none";
+  document.getElementsByClassName('text')[0].style.color = "gray";
+  document.getElementById('read').innerHTML= "<p>READ LESS</p>";
+
+}
+
+function hide(){
+  document.getElementById('read').setAttribute("onclick","show()");
+  document.getElementById('read').setAttribute("class","readClose");
+  document.getElementsByClassName('text')[0].style.maxHeight = "120px";
+  document.getElementsByClassName('text')[0].style.color = "black";
+  document.getElementById('read').innerHTML= "<p>READ MORE</p>";
 }
