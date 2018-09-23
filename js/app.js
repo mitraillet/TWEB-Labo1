0085b5dc9761978a3a281c21863148ed0bab927f
mitraillet <steve.henriquet@gmail.com>
2018-09-23 16:56:42 +0200

    Adding style and a little animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 function follow() {
   alert("You're now following Humans of New York");
 }
+
+function like(){
+  alert("You like this !");
+}
+
+
+function arrow(){
+  alert("You have share it !");
+}
